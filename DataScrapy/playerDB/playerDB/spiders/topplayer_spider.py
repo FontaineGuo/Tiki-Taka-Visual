3206dc0603d333b72217eda4b69b14c656f53b4d
Fontaine_470S_win10 <fontaine_gzy@163.com>
2018-11-02 09:45:36 +0800

    scrapy soccer who

--- DataScrapy/playerDB/playerDB/spiders/topplayer_spider.py
+++ DataScrapy/playerDB/playerDB/spiders/topplayer_spider.py
@@ -2,19 +2,17 @@ import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
+    name = "topplayer"
 
     def start_requests(self):
         urls = [
-            'https://www.whoscored.com/Regions/252/Tournaments/2/Seasons/2458/England-Premier-League',
-            'https://www.whoscored.com/Regions/252/Tournaments/2/Seasons/3853/England-Premier-League',
+            'https://www.whoscored.com/Regions/252/Tournaments/2/Seasons/2458/England-Premier-League'
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.html' % page
+        filename = 'topplayer.html'
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
