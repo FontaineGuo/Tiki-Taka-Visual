82e4dabd6934c223a87b7df205c5faa2947f42c2
Fontaine_470S_win10 <fontaine_gzy@163.com>
2018-11-06 17:20:39 +0800

    goalgetter_spider

--- DataScrapy/playerDB/playerDB/spiders/goalgetter_spider.py
+++ DataScrapy/playerDB/playerDB/spiders/goalgetter_spider.py
@@ -8,19 +8,22 @@ class GoalGetterSpider(scrapy.Spider):
     name = "goalgetter"
     
     def start_requests(self):
-
-       
+        # urls  = (LinkGenerator.generate_goalgetter_links('Ligue-1'))
+        # urls.extend(LinkGenerator.generate_goalgetter_links('SerieA'))
+        # urls.extend(LinkGenerator.generate_goalgetter_links('LaLiga'))
+        # urls.extend(LinkGenerator.generate_goalgetter_links('Bundesliga'))
         urls = []
-        urls.append(LinkGenerator.generate_goalgetter_year_link('PrimerLeague', '2010'))
+        urls.append('https://www.worldfootball.net/goalgetter/esp-primera-division-2016-2017_2/')
         # urls.append('https://www.worldfootball.net/goalgetter/eng-premier-league-2010-2011/')
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        data_name = response.xpath('//*[@id="navi"]/div[3]/h1/text()').extract_first()
-        goalgetter_trs = response.xpath('table.standard_tabelle tr')
+        link_group = response.url.split("/")
+        data_name = link_group[-2] + '-' + link_group[-3]
+        goalgetter_trs = response.css('table.standard_tabelle tr')
         filename = data_name + '.csv'
-        with open(filename, 'wb') as f:
+        with open(filename, 'w', encoding = 'utf-8') as f:
             csv_writer = csv.writer(f, delimiter=',' ,lineterminator='\n', quoting=csv.QUOTE_MINIMAL)
             pattern = r'\((\d{0,2})\)'
             # write title
@@ -31,6 +34,6 @@ class GoalGetterSpider(scrapy.Spider):
                 country = tds[3].css('td::text').extract()
                 team = tds[4].css('a')[1].css('a::text').extract()
                 goals = tds[5].css('b::text').extract()
-                penalty = re.search(pattern, tds[5].css('td::text').extract())[1]
+                penalty = re.search(pattern, str(tds[5].css('td::text').extract()))[1]
 
-                csv_writer.writerow([name, country, team, int(goals), int(penalty)])
+                csv_writer.writerow([str(name[0]), str(country[0]), str(team[0]), int(goals[0]), int(penalty)])
