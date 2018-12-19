d8e75e87a3ae9cff60e561689445277233f7d33f
Fontaine_Lab <fontaine_gzy@163.com>
2018-12-19 19:54:47 +0800

    add season json

--- Project/data_process/DataSrc/process_script/create_team_table_json.py
+++ Project/data_process/DataSrc/process_script/create_team_table_json.py
@@ -1,9 +1,28 @@
 import sqlite3
+import json
+from collections import OrderedDict
 
+years = ['2010-2011','2011-2012','2012-2013','2013-2014','2014-2015',
+        '2015-2016', '2016-2017', '2017-2018']
 conn = sqlite3.connect('..\\league_table.db')
 cursor = conn.cursor()
-year = '2010-2011'
-league = 'bundesliga'
-cursor.execute("SELECT Team from '" + league + "_" + year + "_table'")
-for item in cursor:
-    print(item)
+
+leagues = ['bundesliga', 'laliga','seriea', 'ligue1', 'premierleague']
+
+for league in leagues:
+    team_yearDict = OrderedDict()
+    for year in years:
+        print('processing ' + year)
+        cursor.execute("SELECT Team from '" + league + "_" + year + "_table'")
+        team = []
+        for item in cursor:
+            if str(item[0]) in team_yearDict:
+                temp_list = team_yearDict[str(item[0])]
+                temp_list.append(year)
+                team_yearDict[str(item[0])] = temp_list
+            else:
+                team_yearDict.update({str(item[0]):[]})
+                team_yearDict[str(item[0])].append(year)
+    j = json.dumps(team_yearDict)
+    with open(league + '_team_season.json', 'w') as f:
+        f.write(j)
